3aa57c2ee38aa880e3c264f045d10d90346b0da1
Faris Paxton <faris.paxton@gmail.com>
2014-09-10 13:57:18 -0700

    Add function to sort tag totals

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+var sort = [0];
+
 $(document).ready(function() {
 	$('#input_tag').submit(function(event){
 		// zero out results if previous search has run
@@ -12,13 +14,44 @@ $(document).ready(function() {
 
 var showTagTotals = function(data) {
 	var result = $('.template ol li:first-child').clone();
-	var print = "#" + data.name + " " + numberWithCommas(data.media_count);
+	// var print = "#" + data.name + " " + numberWithCommas(data.media_count);
+	var print = "#" + data.name + " " + data.media_count.toLocaleString();
 	result.text(print);
+	totalSort(data);
 	return result;
 };
 
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+var totalSort = function(num) {
+	if (sort[0] == 0) {
+		sort[0] = num;
+	}
+	else if (sort.length == 30) {
+		console.log("no more tags");
+	}
+	else if (num.media_count > sort[0].media_count) {
+		sort.unshift(num);
+	}
+	else if (num.media_count < sort[sort.length - 1].media_count) {
+		sort.push(num);
+	}
+	else {
+		for (x = 0; x < sort.length; x++) {
+			if (num.media_count > sort[x].media_count) {
+				sort.splice(x, 0, num);
+				break;
+			}
+		}
+	}
+
+
+	for (x = 0; x < sort.length; x++) {
+		console.log(sort[x].media_count);
+	}
+	console.log("new click");
 }
 
 var getTagTotals = function(tag) {
